73b0fba56cdc87ccabc19907d8765075e081ce75
Erertertet <s2293297@ed.ac.uk>
2025-01-15 20:03:41 -0600

    add: requirement, pdf render to img

--- main.py
+++ main.py
@@ -16,19 +16,45 @@ def png_to_base64(filename):
 
             return base64_str
 
-image = 'test.png'
+def img_to_base64(img):
+    img = img.convert('RGB')
+    with io.BytesIO() as output_bytes:
+        img.save(output_bytes, format='JPEG')
+        bytes_data = output_bytes.getvalue()
+        base64_str = base64.b64encode(bytes_data).decode('utf-8')
 
-v_agent = VisionAgent()
-v_agent.append_message(MessageType.IMAGE_BASE64, png_to_base64(image))
-v_agent.append_message(MessageType.TEXT, 'recognize the text and response in formatted html, not to include the name of the book at the top')
-res = v_agent.chat()
+        return base64_str
 
-c_agent = SimpleChatAgent()
-res = c_agent.chat("only keep the content, remove header and style of the html, not to include ``` container, center formulae using text-align center: %s" % res)
+from pdf2image import convert_from_path, convert_from_bytes
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
+import tempfile
 
-with open('out.html', 'w', encoding='utf-8') as file:
-    with open('html/template.html') as temp:
-        s = temp.read() % (image, res)
-        file.write(s)
+imgs = convert_from_path('pdf/test.pdf', fmt='jpeg', thread_count=4)
+print(imgs)
+
+out = open('out.html', 'w', encoding = 'utf-8')
+
+cnt = 0
+
+for img in imgs:
+    v_agent = VisionAgent()
+    v_agent.append_message(MessageType.IMAGE_BASE64, img_to_base64(img))
+    v_agent.append_message(MessageType.TEXT, 'recognize the text and response in formatted html, not to include the name of the book at the top')
+    res = v_agent.chat()
+
+    c_agent = SimpleChatAgent()
+    res = c_agent.chat("only keep the content, remove header and style of the html, not to include ``` container, center formulae using text-align center: %s" % res)
+
+    # with open('out.html', 'w', encoding='utf-8') as file:
+    #     with open('html/template.html') as temp:
+    #         s = temp.read() % (image, res)
+    #         file.write(s)
+
+    print(res)
+    out.write(res)
 
 webbrowser.open('out.html')
